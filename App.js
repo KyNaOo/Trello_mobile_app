5ff1f3c618722855ed275cc675bf7865afda557d
ndaen <daennoah@gmail.com>
2024-02-27 19:16:20 +0100

    feat: topbar and splash screené

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import TopBar from "./component/TopBar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+          <TopBar />
+        <ScrollView>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </ScrollView>
+      </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ component/TopBar.jsx
@@ -0,0 +1,51 @@
+import React, {useCallback} from 'react';
+import {View, StyleSheet, Image, Text} from "react-native";
+import {useFonts} from "expo-font";
+
+const fonts = {
+    'Chillax-regular': "https://api.fontshare.com/v2/css?f[]=chillax@400&display=swap"
+}
+function TopBar(props) {
+    const [fontsLoaded] = useFonts({
+        'Chillax': require("../assets/fonts/Chillax-Regular.otf"),
+    })
+
+    if (fontsLoaded) {
+        return (
+            <View style={styles.topBar}>
+                <Image style={styles.img} source={require("../assets/logo_flowy.png")} />
+                <Text style={styles.title}>Flowy</Text>
+            </View>
+        );
+    } else {
+        return null;
+    }
+}
+
+
+const styles = StyleSheet.create({
+    topBar: {
+        top: 0,
+        width: "100%",
+        backgroundColor: "#00171f",
+        height: 120,
+        display: "flex",
+        flexDirection:"row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        textAlign: "center",
+        paddingTop: 50,
+    },
+    img: {
+        width: 64,
+        height: 64,
+    },
+    title: {
+        fontSize: 24,
+        color: "#fff",
+        paddingRight:20,
+        fontFamily: 'Chillax',
+    }
+
+})
+export default TopBar;
