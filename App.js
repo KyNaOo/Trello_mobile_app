c4454a3d483cc176fb2f4976bb7ce9dca145ceb0
Ethanol <ethanbellaiche0@gmail.com>
2024-02-27 19:09:53 +0100

    feat: display buttons in homepage with the names of the boards

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from "react";
 
 export default function App() {
+  const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+  const apiToken = process.env.EXPO_PUBLIC_API_TOKEN;
+  const [dataUser, setDataUser] = useState([]);
+  const [dataBoard, setDataBoard] = useState([]);
+
+  const getBoards = async ()=>{
+    let boards = [];
+    for (const e of dataUser.idBoards) {
+      const urlBoard = `https://api.trello.com/1/boards/${e}?key=${apiKey}&token=${apiToken}`;
+      let response = await fetch(urlBoard);
+      boards.push(await response.json())
+    }
+    setDataBoard(boards);
+  };
+  const getConnectedUser = async () => {
+    const urlMe = `https://api.trello.com/1/members/me/?key=${apiKey}&token=${apiToken}`
+    let result = await fetch(urlMe);
+    let response = await result.json();
+    setDataUser(response);
+    getBoards()
+  }
+  useEffect(() => {
+    getConnectedUser();
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {
+        dataBoard ?
+            <View>
+              <Text>{dataBoard.map((board) => {
+
+                return (<Button title={board.name}></Button>);
+              })}</Text>
+            </View>
+            :
+            <Text>Vous n'avez pas de tableau</Text>
+      }
       <StatusBar style="auto" />
     </View>
   );
